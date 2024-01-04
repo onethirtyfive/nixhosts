47d208825205320dcaabab65227a77d7701e19e0
Joshua Morris <joshua.morris@hey.com>
2024-01-04 17:57:46 -0600

    docker support

--- hosts/neutrino/configuration.nix
+++ hosts/neutrino/configuration.nix
@@ -96,6 +96,8 @@
     HandlePowerKey=suspend
   '';
 
+  virtualisation.docker.enable = true;
+
   # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "23.11";
 }

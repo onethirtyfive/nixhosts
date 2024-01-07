a5691dcf46e9c48a0c7d3ef2a290b25e191b9e36
Joshua Morris <joshua.morris@hey.com>
2024-01-07 11:10:40 -0600

    monitor swap

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -39,8 +39,8 @@ in
       ];
 
       monitor = [
-        "HDMI-A-1, 2560x1440@60, 1440x560, 1"
-        "HDMI-A-2, 2560x1440@60, 0x0, 1, transform, 1"
+        "HDMI-A-1, 2560x1440@60, 0x0, 1"
+        "HDMI-A-2, 2560x1440@60, 2560x-560, 1, transform, 3"
         ",preferred,auto,1"
       ];
 

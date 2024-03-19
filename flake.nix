cfda07e9e6c97f5f3407642ca5b6835a6d815448
Joshua Morris <joshua.morris@hey.com>
2024-03-18 21:12:59 -0500

    onethirtyfive-neovim no longer follows nixpkgs

--- flake.nix
+++ flake.nix
@@ -18,8 +18,9 @@
       flake = false;
     };
 
+    # This does not follow nixpkgs for now.
     onethirtyfive-neovim.url = "github:onethirtyfive/neovim-nix";
-    onethirtyfive-neovim.inputs.nixpkgs.follows = "nixpkgs";
+    # onethirtyfive-neovim.inputs.nixpkgs.follows = "nixpkgs";
 
     ml4w.url = "gitlab:onethirtyfive/ml4w-dotfiles-nixos";
     ml4w.inputs.nixpkgs.follows = "nixpkgs";

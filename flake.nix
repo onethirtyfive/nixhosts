2fa6cc87d17e5fa1f09a3574a73e410d67196144
Joshua Morris <joshua.morris@hey.com>
2024-09-21 18:26:15 -0500

    works: nixpkgs-darwin follows darwin/nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,11 +3,10 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     darwin.url = "github:lnl7/nix-darwin";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs-darwin.follows = "darwin/nixpkgs";
 
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";

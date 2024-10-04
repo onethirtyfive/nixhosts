93e0db62683dd7f857d9fd4db2ad6c91c7e34e06
Joshua Morris <joshua.morris@hey.com>
2024-10-04 00:40:26 -0500

    all: skeleton devenv

--- flake.nix
+++ flake.nix
@@ -2,6 +2,8 @@
   description = "All host configurations for Joshua's computers.";
 
   inputs = {
+    systems.url = "github:nix-systems/default";
+
     # macos
     darwin.url = "github:lnl7/nix-darwin";
     nixpkgs-darwin.follows = "darwin/nixpkgs";
@@ -9,33 +11,47 @@
     home-manager-darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
     mac-app-util.url = "github:hraban/mac-app-util";
     mac-app-util.inputs.nixpkgs.follows = "nixpkgs-darwin";
+    devenv-darwin.url = "github:cachix/devenv";
+    devenv-darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
 
     # linux
     home-manager.url = "github:nix-community/home-manager";
     nixpkgs.follows = "home-manager/nixpkgs";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
 
     # common
     rust-overlay.url = "github:oxalica/rust-overlay";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
+    onethirtyfive-neovim.url = "github:onethirtyfive/neovim-nix";
     alacritty-theme.url = "github:alacritty/alacritty-theme";
     alacritty-theme.flake = false;
-    onethirtyfive-neovim.url = "github:onethirtyfive/neovim-nix";
   };
 
   outputs = inputs@{
       self
-    , nixpkgs-darwin
-    , nixpkgs
+    , systems
+
+    # macos
     , darwin
-    , home-manager
+    , nixpkgs-darwin
     , home-manager-darwin
     , mac-app-util
-    , onethirtyfive-neovim
+    , devenv-darwin
+
+    # nixos
+    , nixpkgs
+    , devenv
+
+    # common
     , rust-overlay
     , nixos-hardware
+    , onethirtyfive-neovim
     , ...
   }:
   let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+
     overlays = [
       rust-overlay.overlays.default
       onethirtyfive-neovim.overlays.default
@@ -43,8 +59,8 @@
     ];
   in  {
     nixConfig = {
-      extra-trusted-public-keys = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= onethirtyfive.cachix.org-1:w+zBnwl7vHfxNHawEN6Ej2zQ2ejgi8oqCxqVZ8wGYCg=";
-      extra-substituters = "https://nix-community.cachix.org https://onethirtyfive.cachix.org";
+      extra-trusted-public-keys = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= onethirtyfive.cachix.org-1:w+zBnwl7vHfxNHawEN6Ej2zQ2ejgi8oqCxqVZ8wGYCg= devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+      extra-substituters = "https://nix-community.cachix.org https://onethirtyfive.cachix.org https://devenv.cachix.org";
     };
 
     darwinConfigurations =
@@ -115,6 +131,33 @@
 
         neutrino = meadowlark; # stepping stone
       };
+
+    packages = forEachSystem (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+    });
+
+    devShells = forEachSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              {
+                # https://devenv.sh/reference/options/
+                packages = [ pkgs.hello ];
+
+                enterShell = ''
+                  hello
+                '';
+
+                processes.hello.exec = "hello";
+              }
+            ];
+          };
+        });
   };
 }
 

288a1f54c1f945ff4718e612ed8db6a2a45b787c
Joshua Morris <joshua.morris@hey.com>
2025-06-16 01:48:27 -0700

    macos: remove alacritty (use Ghostty via homebrew)

--- hm-modules/macos/default.nix
+++ hm-modules/macos/default.nix
@@ -1,5 +1,4 @@
 [
-  ./alacritty.nix
   ./meta.nix
   ./zsh
 ]
--- modules/nix-darwin/security.nix
+++
@@ -1,5 +0,0 @@
-{ ... }:
-{
-  # Add ability to used TouchID for sudo authentication
-  security.pam.enableSudoTouchIdAuth = false;
-}

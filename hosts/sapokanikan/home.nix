954cdfdbb7afa017880730b0b704e4beeb23824a
Joshua Morris <joshua.morris@hey.com>
2025-06-16 20:01:52 -0700

    sapokanikan: remove karabiner config

--- hosts/sapokanikan/home.nix
+++ hosts/sapokanikan/home.nix
@@ -1,21 +1,3 @@
 { pkgs, ... }:
 {
-  home.file.".config/karabiner" =
-    let
-      inherit (pkgs) stdenv;
-
-      configStore = stdenv.mkDerivation {
-        name = "karabiner-config-store";
-        src = ./dotconfig;
-
-        installPhase = ''
-          mkdir -p $out
-          cp -r $src/karabiner $out/.
-        '';
-      };
-    in
-    {
-      source = "${configStore}/karabiner";
-      target = ".config/karabiner";
-    };
 }

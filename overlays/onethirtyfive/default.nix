89d977baafb09ed774d319a13300515a0d36c541
Joshua Morris <joshua.morris@hey.com>
2025-11-10 22:20:59 -0800

    disable userwide python

--- overlays/onethirtyfive/default.nix
+++ overlays/onethirtyfive/default.nix
@@ -1,6 +1,6 @@
 self: super: {
   onethirtyfive = super.onethirtyfive // {
-    python3 = self.callPackage ./python3 { };
+    # python3 = self.callPackage ./python3 { };
     ruby = self.callPackage ./ruby { };
   };
 }
